b702197a9f9c02316b4ec09f074efc8950f7110b
cukadarMert <cukadar.mertkaan@gmail.com>
2024-02-28 22:13:39 +0300

    test page logger updates

--- Detect-Pos/Screens/TestPage/testPage.js
+++ Detect-Pos/Screens/TestPage/testPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Pressable, Dimensions } from 'react-native';
+import { useSensor } from '../../Component/Sensors';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -7,6 +8,9 @@ const screenHeight = Dimensions.get('window').height;
 export default function Test() {
   const [leftPressed, setLeftPressed] = useState(false);
   const [rightPressed, setRightPressed] = useState(false);
+
+  const sensorData = useSensor();
+
 
   const handleLeftPress = () => {
     setLeftPressed(true);
@@ -20,20 +24,35 @@ export default function Test() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.valuesWrapper}>
+      <Text style={{fontWeight: "bold"}}>Accelerometer</Text>
       <View style={styles.values}>
-        <Text>X : </Text>
-        <Text>Y : </Text>
-        <Text>Z : </Text>
+        <Text>X : {sensorData.accelerometerData.x}</Text>
+        <Text>Y : {sensorData.accelerometerData.y}</Text>
+        <Text>Z : {sensorData.accelerometerData.z}</Text>
+      </View>
+      <Text style={{fontWeight: "bold"}}>Gyroscope</Text>
+      <View style={styles.values}>
+        <Text>X : {sensorData.gyroscopeData.x}</Text>
+        <Text>Y : {sensorData.gyroscopeData.y}</Text>
+        <Text>Z : {sensorData.gyroscopeData.z}</Text>
+      </View>
+      <Text style={{fontWeight: "bold"}}>Magnetometer</Text>
+      <View style={styles.values}>
+        <Text>X : {sensorData.magnetometerData.x}</Text>
+        <Text>Y : {sensorData.magnetometerData.y}</Text>
+        <Text>Z : {sensorData.magnetometerData.z}</Text>
+      </View>
       </View>
       <View style={styles.push}>
         <Pressable
-          style={[styles.Pressable, { backgroundColor: leftPressed ? "#E26EE5" : "#711DB0" }]}
+          style={[styles.Pressable, { backgroundColor: leftPressed ? "#711DB0" : "purple"}]}
           onPress={handleLeftPress}
         >
           <Text style={styles.textVal}>Left</Text>
         </Pressable>
         <Pressable
-          style={[styles.Pressable, { backgroundColor: rightPressed ? "#E26EE5" : "#711DB0" }]}
+          style={[styles.Pressable, { backgroundColor: rightPressed ? "#711DB0" : "purple" }]}
           onPress={handleRightPress}
         >
           <Text style={styles.textVal}>Right</Text>
@@ -46,7 +65,7 @@ export default function Test() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#F3F8FF",
     alignItems: "center",
     justifyContent: 'flex-end',
   },
@@ -67,13 +86,23 @@ const styles = StyleSheet.create({
     width: screenWidth,
   },
   values: {
-    width: screenWidth,
+    width: screenWidth - 10,
+    borderRadius: 10,
+    padding: 10,
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
+    borderWidth: 1,
+    borderColor: "black",
   },
   textVal: {
     color: "white",
     fontWeight: "bold"
+  },
+  valuesWrapper: {
+    width: screenWidth,
+    height: screenHeight / 3,
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
